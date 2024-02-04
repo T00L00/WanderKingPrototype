ff5646384942eb1ef055d3c615827186df06dff3
TooLoo <17146137+T00L00@users.noreply.github.com>
2024-02-04 17:37:59 -0600

    Ajusted formation algorithm to evenly distribute units in row. Basic working solution. Still needs for work later down the line.

--- Assets/Scripts/FormationGenerator.cs
+++ Assets/Scripts/FormationGenerator.cs
@@ -19,26 +19,27 @@ namespace WK
         public List<Vector3> GeneratePositions(int followerCount)
         {
             groupLength = Mathf.CeilToInt((float)followerCount / groupWidth);
+            int unitsInLastRow = followerCount % groupWidth;
+            if (unitsInLastRow == 0 && followerCount > 0) unitsInLastRow = groupWidth;  // If the last row is full or there's only one row that's full.
 
-            Vector3 middleOffset = new Vector3(groupWidth * 0.5f, 0, groupLength * 0.5f);
+            Vector3 middleOffset = new Vector3((groupWidth - 1) * spread * 0.5f, 0, (groupLength - 1) * spread * 0.5f);
 
-            List<Vector3> positions = new();
+            List<Vector3> positions = new List<Vector3>();
 
-            for (int x = 0; x < groupWidth; x++)
+            for (int z = 0; z < groupLength; z++)
             {
-                for (int z = 0; z < groupLength; z++)
+                int unitsInThisRow = (z == groupLength - 1) ? unitsInLastRow : groupWidth;
+                float spacing = (unitsInThisRow > 1) ? (groupWidth - 1) * spread / (unitsInThisRow - 1) : 0;
+
+                for (int x = 0; x < unitsInThisRow; x++)
                 {
-                    if (hollow && 
-                        x != 0 && 
-                        x != groupWidth - 1 && 
-                        z != 0 && 
-                        z != groupLength - 1) 
+                    if (hollow && z != 0 && z != groupLength - 1 && x != 0 && x != unitsInThisRow - 1)
                         continue;
 
-                    Vector3 pos = new Vector3(x + (z % 2 == 0 ? 0 : nthOffset), 0, z - followDistance);
+                    float offsetX = (unitsInThisRow > 1) ? x * spacing : (groupWidth - 1) * spread * 0.5f;
+                    Vector3 pos = new Vector3(offsetX + (z % 2 == 0 ? 0 : nthOffset), 0, z * spread - followDistance);
                     pos -= middleOffset;
                     pos += GetNoise(pos);
-                    pos *= spread;
 
                     pos = transform.parent.TransformPoint(pos);
                     positions.Add(pos);
