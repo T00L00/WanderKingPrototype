a718b1174c0e50218164f7842e0b951ec62e9b83
TooLoo <17146137+T00L00@users.noreply.github.com>
2024-02-18 13:00:41 -0600

    Remove comments

--- Assets/Scripts/Launching/Chambering.cs
+++ Assets/Scripts/Launching/Chambering.cs
@@ -30,20 +30,15 @@ namespace WK
 
         IEnumerator MoveToPosition(Vector3 target, float duration)
         {
-            Vector3 startPosition = activeFollower.UnitParent.localPosition; // Store the starting position
-            float startTime = Time.time; // Remember the start time
+            Vector3 startPosition = activeFollower.UnitParent.localPosition;
+            float startTime = Time.time;
             float fraction = 0;
 
 
             while (fraction < 1)
             {
-                // Calculate the fraction of the total duration that has passed
                 fraction = (Time.time - startTime) / duration;
-
-                // Update the GameObject's position
                 activeFollower.UnitParent.localPosition = Vector3.Lerp(startPosition, target, fraction);
-
-                // Yield until the next frame
                 yield return null;
             }
         }
